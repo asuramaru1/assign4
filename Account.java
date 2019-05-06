7ec3d3501d80c38c2170c8b004818995c587e0d0
asuramaru1 <gkhar17@freeuni.edu.ge>
2019-05-07 00:05:59 +0400

    bank

--- Account.java
+++ Account.java
@@ -1,4 +1,39 @@
-package PACKAGE_NAME;
-
 public class Account {
+    private double balance;
+    private int transactionsMade;
+    private int id;
+
+    public void Account(int ind, double balance, int transactionsMade) {
+        this.transactionsMade = transactionsMade;
+        this.id = id;
+        this.balance = balance;
+    }
+
+    public void Account(int id) {
+        Account(id, 1000, 0);
+    }
+
+    public synchronized void addBalance(double add) {
+        balance += add;
+        transactionsMade++;
+    }
+
+    public synchronized double getBalance() {
+        return balance;
+    }
+
+    public synchronized int getTransactionsMade() {
+        return transactionsMade;
+    }
+
+    public synchronized void withdraw(double amount) {
+        assert amount <= balance;
+        balance -= amount;
+        transactionsMade++;
+    }
+
+    @Override
+    public synchronized String toString() {
+        return "account:  " + id + "  balance : " + balance + "  trans: " + transactionsMade;
+    }
 }
--- transaction.java
+++ transaction.java
@@ -1,4 +1,23 @@
-package PACKAGE_NAME;
-
 public class transaction {
+    private final int from;
+    private final int to;
+    private final int amount;
+
+    public transaction(int from, int to, int amount) {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public int getFrom() {
+        return from;
+    }
+
+    public int getTo() {
+        return to;
+    }
 }
