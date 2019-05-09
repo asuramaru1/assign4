2d27b8bb1fcf79e7c0ebd4f91a2ba7d50d8f38ed
asuramaru1 <gkhar17@freeuni.edu.ge>
2019-05-09 20:22:02 +0400

    asd

--- Account.java
+++ Account.java
@@ -3,29 +3,36 @@ public class Account {
     private int transactionsMade;
     private int id;
 
+
     public Account(int ind, double balance, int transactionsMade) {
         this.transactionsMade = transactionsMade;
         this.id = id;
         this.balance = balance;
     }
+
+
     public  Account(int id) {
         this.transactionsMade = 0;
         this.id = id;
         this.balance = 1000;
     }
 
+
     public synchronized void addBalance(double add) {
         balance += add;
         transactionsMade++;
     }
 
+
     public synchronized double getBalance() {
         return balance;
     }
 
+
     public synchronized int getTransactionsMade() {
         return transactionsMade;
     }
+
 
     public synchronized void withdraw(double amount) {
         //if (!(amount <= balance)) throw new AssertionError();
@@ -33,8 +40,11 @@ public class Account {
         transactionsMade++;
     }
 
+
     @Override
     public synchronized String toString() {
         return "account:  " + id + "  balance : " + balance + "  trans: " + transactionsMade;
     }
+
+
 }
